62add2259a209f66d69c863cc1d221480895f2c6
Tom Keschl <thomas.keschl@wexinc.com>
2018-01-29 22:40:08 -0500

    Simplify script and add SSH and http_proxy switching in .zshrc.

--- autolocation.sh
+++ autolocation.sh
@@ -34,16 +34,14 @@
 # Locations which we should try to detect.
 
 # Home
+declare -a LOC_NAME
+declare -a LOC_SSID
 LOC_NAME[0]="Home"
-LOC_SSID[0]="home_network"
-LOC_EN0IP[0]="10.0.1.41"
-LOC_EN1IP[0]="10.0.1.41"
+LOC_SSID[0]="fupa"
 
 # Work
-#LOC_NAME[1]="Work"
-#LOC_SSID[1]="work_network"
-#LOC_EN0IP[1]=
-#LOC_EN1IP[1]=
+LOC_NAME[1]="Work"
+LOC_SSID[1]="WEX-Inc"
 #---------------------------------------------------------------------
 
 SELF=$(echo $0 | sed 's#^.*/##')
@@ -69,17 +67,17 @@ LOGGER="/usr/bin/logger"
 # scselect(8)
 SCSELECT="/usr/sbin/scselect"
 
-# growlnotify
-GROWL="/usr/local/bin/growlnotify"
+# notifier
+NOTIFY="/usr/bin/osascript"
 
-if [ ! -x $SCSELECT ] || [ ! -x $LOGGER ] || [ ! -x $GROWL ]; then
+if [ ! -x ${SCSELECT} ] || [ ! -x ${LOGGER} ] || [ ! -x ${NOTIFY} ]; then
     echo ""
     echo "$SELF - FATAL"
     echo "The following binaries are required and were not found:"
     echo ""
     echo "  $SCSELECT"
     echo "  $LOGGER"
-    echo "  $GROWL"
+    echo "  $NOTIFY"
     echo ""
     echo "ABORTING."
     echo ""
@@ -109,12 +107,12 @@ function get_ssid ()
 {
     airport="/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport -I"
 
-    ssid=$($airport 2>> >($LOGGER -p $ERRLOG) \
+    ssid=$(${airport} 2>> >(${LOGGER} -p ${ERRLOG}) \
 	| grep ' SSID:' \
 	| cut -d ':' -f 2 \
 	| tr -d ' ')
 
-    eval "$1=$ssid"
+    eval "$1=${ssid}"
 }
 
 #---------------------------------------------------------------------
@@ -129,11 +127,11 @@ function get_ssid ()
 function get_location ()
 {
     # Grab the location, send any errors from scselect to logger
-    location=$($SCSELECT 2>> >($LOGGER -p $ERRLOG) \
+    location=$(${SCSELECT} 2>> >(${LOGGER} -p ${ERRLOG}) \
 	| grep '^ \* ' \
 	| sed 's/.*(\(.*\))/\1/')
 
-    eval "$1=$location"
+    eval "$1=${location}"
 }
 
 #---------------------------------------------------------------------
@@ -149,22 +147,37 @@ function set_location ()
     location=$1
 
     # Log the activity
-    $LOGGER -p $INFOLOG "Setting Network Location to: $location"
+    ${LOGGER} -p ${INFOLOG} "Setting Network Location to: $location"
 
     # Set the location and send scselect output to logger
-    $SCSELECT $location 1>> >($LOGGER -p $INFOLOG) 2>> >($LOGGER -p $ERRLOG)
+    ${SCSELECT} ${location} 1>> >(${LOGGER} -p ${INFOLOG}) 2>> >(${LOGGER} -p ${ERRLOG})
 
     # If scselect failed, alert and bail.
     if [ ${?} -ne 0 ]; then
 
-	$LOGGER -p $ERRLOG "Error selecting Location $location!"
+        ${LOGGER} -p ${ERRLOG} "Error selecting Location $location!"
 
-	$GROWL -s -p2 -n "Auto Location" \
-	    -a "/Applications/Utilities/Airport Utility.app/" \
-	    -m "ERROR: Unable to set Location: $location"
+        ${NOTIFY} -e 'display notification "ERROR: Unable to set Location: '"${location}"'"'
 
-	exit 1
+        exit 1
 
+    fi
+
+    # Swap ssh configs
+    if [ -f ~/.ssh/config.${location} ] ; then
+        ${LOGGER} -p ${INFOLOG} "Swapping SSH config to $location..."
+        rm ~/.ssh/config
+        ln -s ~/.ssh/config.${location} ~/.ssh/config
+    fi
+
+    # What's the previous location?
+    previousLocation=$2
+
+    # Swap zsh configs
+    if [ -f ~/.${location}rc ] ; then
+        ${LOGGER} -p ${INFOLOG} "Adding zsh config section for $location..."
+        sed -i.bak0 '/#'"$previousLocation"/',/#\/'"$previousLocation"'/d' ~/.zshrc
+        sed -i.bak1 '/#Begin'"$location"'Section/r .'"$location"'rc' ~/.zshrc
     fi
 }
 
@@ -175,45 +188,28 @@ function set_location ()
 # Grab our current location
 CUR_LOC=
 get_location CUR_LOC
-$LOGGER -p $INFOLOG "Current location is: $CUR_LOC"
+${LOGGER} -p ${INFOLOG} "Current location is: $CUR_LOC"
 
 # Get the associated SSID
 SSID=
 get_ssid SSID
 if [ -n "$SSID" ]; then
-    $LOGGER -p $INFOLOG "Associated to SSID $SSID"
+    ${LOGGER} -p ${INFOLOG} "Associated to SSID $SSID"
 else
-    $LOGGER -p $INFOLOG "Not associated to any SSID"
+    ${LOGGER} -p ${INFOLOG} "Not associated to any SSID"
 fi
-
-# Get the ethernet IP address
-EN0IP=$(ifconfig en0 | grep -w inet | cut -d ' ' -f 2)
-$LOGGER -p $INFOLOG "en0 IP: $EN0IP"
-
-# Get the WiFi IP address
-EN1IP=$(ifconfig en1 | grep -w inet | cut -d ' ' -f 2)
-$LOGGER -p $INFOLOG "en1 IP: $EN1IP"
 
 # Figure out which location we need to select based on the current
 # network status.
 NEW_LOC=
 REASON=
 i=0;
-while [ $i -lt ${#LOC_NAME[@]} ]; do
+while [ ${i} -lt ${#LOC_NAME[@]} ]; do
     if [ "$SSID" == "${LOC_SSID[$i]}" ]; then
 	NEW_LOC="${LOC_NAME[$i]}"
 	REASON="Matched WiFi"
-	break
-    elif [ -n "$EN0IP" && "$EN0IP" == "${LOC_EN0IP[$i]}" ]; then
-	NEW_LOC="${LOC_NAME[$i]}"
-	REASON="Matched en0 IP"
-	break
-    elif [ -n "$EN1IP" && "$EN1IP" == "${LOC_EN1IP[$i]}" ]; then
-	NEW_LOC="${LOC_NAME[$i]}"
-	REASON="Matched en1 IP"
-	break
     fi
-    i=$(expr $i + 1)
+    i=$(expr ${i} + 1)
 done
 
 # If we didn't find a configured location to match the current network
@@ -224,64 +220,44 @@ if [ -z "$NEW_LOC" ]; then
 fi
 
 # At this point we have a decision as to what we want to do.
-$LOGGER -p $INFOLOG "Selected $NEW_LOC as desired location."
+${LOGGER} -p ${INFOLOG} "Selected $NEW_LOC as desired location."
 
 # If the location isn't changing, do nothing.
 if [ "$NEW_LOC" == "$CUR_LOC" ]; then
-    $LOGGER -p $INFOLOG "Location has not changed. Reason: $REASON"
+    ${LOGGER} -p ${INFOLOG} "Location has not changed. Reason: $REASON"
     exit 0
 fi
 
 # If the location is changing, figure out what we need to change it to
 # and set the new location.
 changed_loc=0
-if [ "$NEW_LOC" == "Automatic" ]; then
-    # We're switching to Automatic
-    $LOGGER -p $INFOLOG "Setting Network Location $NEW_LOC"
-    set_location $NEW_LOC
-    changed_loc=1
-else
-    # We're switching to a different location as defined up top.
-    i=0;
-    while [ $i -lt ${#LOC_NAME[@]} ]; do
-	if [ "$NEW_LOC" == "${LOC_NAME[$i]}" ]; then
-	    $LOGGER -p $INFOLOG "Setting Network Location $NEW_LOC"
-	    set_location $NEW_LOC
-	    changed_loc=1
-	    break
-	fi
-	i=$(expr $i + 1)
-    done
-fi
+
+${LOGGER} -p ${INFOLOG} "Setting Network Location $NEW_LOC"
+set_location ${NEW_LOC} ${CUR_LOC}
+changed_loc=1
 
 # Check the current location again to see if it's changed
 CUR_LOC=
 get_location CUR_LOC
-$LOGGER -p $INFOLOG "Current location is now: $CUR_LOC"
+${LOGGER} -p ${INFOLOG} "Current location is now: $CUR_LOC"
 
 # If we think we've changed network locations...
-if [ $changed_loc -eq 1 ]; then
+if [ ${changed_loc} -eq 1 ]; then
 
     # See if we really did change...
     if [ "$CUR_LOC" == "$NEW_LOC" ]; then
 
-	# Yay, announce our success with pride!
-	$GROWL -s -p2 -n "Auto Location" \
-	    -a "/Applications/Utilities/Airport Utility.app/" \
-	    -m "Updated Location: $NEW_LOC Reason: $REASON SSID: $SSID"
+        # Yay, announce our success with pride!
+        ${NOTIFY} -e 'display notification "Updated Location: '"$NEW_LOC"' Reason: '"$REASON"' SSID: '"$SSID"'"'
 
-	$LOGGER -p $INFOLOG "Updated Location: $NEW_LOC; REASON: $REASON; SSID: $SSID"
+        ${LOGGER} -p ${INFOLOG} "Updated Location: $NEW_LOC; REASON: $REASON; SSID: $SSID"
 
     else
 
-	# Boo, we completely failed, and need to tell someone.
-	$GROWL -s -p2 -n "Auto Location" \
-	    -a "/Applications/Utilities/Airport Utility.app/" \
-	    -m "FAILED! Unable to update Location to $NEW_LOC! Reason: $REASON SSID: $SSID"
+        # Boo, we completely failed, and need to tell someone.
+        ${NOTIFY} -e 'display notification "FAILED! Unable to update Location to '"$NEW_LOC"'! Reason: '"$REASON"' SSID: '"$SSID"
 
-	$LOGGER -p $ERRLOG "FAILED! Unable to update Location to $NEW_LOC!"
+        ${LOGGER} -p ${ERRLOG} "FAILED! Unable to update Location to $NEW_LOC!"
     fi
 
 fi
-
-# vim: ft=sh sw=4
